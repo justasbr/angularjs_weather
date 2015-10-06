d9ccc0d2372eb03f2e5b27c718ff84197a9c29a7
Justas Brazauskas <justasbr@gmail.com>
2015-10-06 01:56:45 +0100

    Struggling with gulp

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,10 @@ var sass = require('gulp-sass');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
+var clean = require('gulp-clean');
+var connect = require('gulp-connect');
+var htmlreplace = require('gulp-html-replace');
+var livereload = require('gulp-livereload');
 
 // Lint Task
 gulp.task('lint', function() {
@@ -14,11 +18,22 @@ gulp.task('lint', function() {
         .pipe(jshint.reporter('default'));
 });
 
+gulp.task('clean', function() {
+    gulp.src('./dist/*')
+      .pipe(clean({force: true}));
+});
+
+gulp.task('copy-bower-components', function () {
+  gulp.src('./bower_components/**')
+    .pipe(gulp.dest('dist/bower_components'));
+});
+
 // Compile Our Sass
 gulp.task('sass', function() {
     return gulp.src('app/*.scss')
         .pipe(sass())
-        .pipe(gulp.dest('dist'));
+        .pipe(gulp.dest('dist'))
+        .pipe(livereload());
 });
 
 // Concatenate & Minify JS
@@ -26,16 +41,35 @@ gulp.task('scripts', function() {
     return gulp.src('app/*.js')
         .pipe(concat('all.js'))
         .pipe(gulp.dest('dist'))
-        .pipe(rename('all.min.js'))
-        .pipe(uglify())
-        .pipe(gulp.dest('dist'));
+        .pipe(livereload());
+});
+
+gulp.task('html-replace', function() {
+  gulp.src('app/index.html')
+    .pipe(htmlreplace({
+        'css': 'index.css',
+        'js': 'all.js'
+    }))
+    .pipe(gulp.dest('dist/'))
+    .pipe(livereload());
 });
 
 // Watch Files For Changes
 gulp.task('watch', function() {
+	livereload.listen();
     gulp.watch('app/*.js', ['lint', 'scripts']);
     gulp.watch('app/*.scss', ['sass']);
+    gulp.watch('app/*.html', ['html-replace']);
+});
+
+gulp.task('connectDist', function () {
+  connect.server({
+    root: 'dist/',
+    port: 9876
+  });
 });
 
 
-gulp.task('default', ['lint', 'sass', 'scripts', 'watch']);
+
+gulp.task('default', ['lint', 'clean', 'copy-bower-components',
+  'sass', 'scripts', 'html-replace', 'watch', 'connectDist']);
